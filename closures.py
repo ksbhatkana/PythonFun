1619276d0a4fc7c898fe76f894e084535e488bf2
KUMARASUBRAHMANYA H <ksbhatfromkana00717@gmail.com>
2022-07-14 19:24:01 +0530

    Note added

--- closures.py
+++ closures.py
@@ -8,3 +8,8 @@ print(cube(4))
 
 square = power_number(2)
 print(square(5))
+
+
+
+#This is possible in Python since Python functions are first class. 
+#It means that Python treats functions as values, so you can assign a function to a variable, pass it as a function argument or return it by another function.
